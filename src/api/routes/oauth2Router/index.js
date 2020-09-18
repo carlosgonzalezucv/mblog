1ec54ae0aa5968786b7121d182220809256828cc
nelsonrivas <nelson.rivas.ucv@gmail.com>
2020-09-17 20:48:02 -0400

    Added route oauth to wordpress

--- src/api/routes/oauth2Router/index.js
+++ src/api/routes/oauth2Router/index.js
@@ -3,6 +3,7 @@ import { OauthRedirectController } from '../../controllers/OauthRedirectControll
 import { FacebookCallbackController } from '../../controllers/FacebookCallbackController';
 import { InstagramRedirectController } from "../../controllers/InstagramRedirectContoller";
 import { InstagramCallbackController } from "../../controllers/InstagramCallbackController";
+import { WordpressRedirectController } from "../../controllers/WordpressRedirectController";
 
 const oauth2Router = Router();
 
@@ -10,6 +11,8 @@ const oauthRedirectController = new OauthRedirectController();
 const facebookCallbackController = new FacebookCallbackController();
 const instagramRedirectController = new InstagramRedirectController();
 const instagramCallbackController = new InstagramCallbackController();
+const wordpressRedirectController = new WordpressRedirectController();
+
 
 oauth2Router.route('/facebook').get((req, res) => oauthRedirectController.execute(req, res));
 oauth2Router.route('/facebook/callback').get((req, res) => facebookCallbackController.execute(req, res));
@@ -20,5 +23,14 @@ oauth2Router.route('/instagram').get((req, res) => instagramRedirectController.e
 
 // Definition: 'api/v1/oauth2/instagram/callback'
 oauth2Router.route('/instagram/callback').get( (req, res) => instagramCallbackController.execute(req, res));
+
+// Wordpress
+// Definition: 'api/v1/oauth2/wordpress'
+oauth2Router.route('/wordpress').get((req, res) => wordpressRedirectController.execute(req, res));
+
+// Definition: 'api/v1/oauth2/wordpress/callback'
+oauth2Router.route('/wordpess/callback').get( (req, res) => {
+    console.log("executing callback");
+})
 
 export { oauth2Router };
